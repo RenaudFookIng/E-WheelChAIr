5fb36a62cce4228075f42bfa25b33b29d479cad4
Renaud JANET <renaud.janet@etu.sorbonne-universite.fr>
2025-10-30 16:13:31 +0100

    Initial commit: First Build ROS2 project for E-WheekChAIr

---
+++ src/sabertooth_controller/build/sabertooth_controller/share/sabertooth_controller/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/media/renaud/Blade 15 HDD/M2_Cours/Designing/sabertooth_ws/src/sabertooth_controller/build/sabertooth_controller"
