134d5ed28f3c2b0b57ec04530d9257e51dfbe4cf
Renaud JANET <renaud.janet@etu.sorbonne-universite.fr>
2025-11-01 17:44:38 +0100

    Ajout du contenu de EWheelChAIr_ws/

---
+++ EWheelChAIr_ws/.history/src/arduino_interface/setup_20251030154012.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'arduino_interface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='renaud',
+    maintainer_email='renaud@todo.todo',
+    description='ODO: Package description',
+    license='Apache License 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
