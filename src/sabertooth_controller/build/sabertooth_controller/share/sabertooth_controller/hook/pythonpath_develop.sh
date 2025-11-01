ef076f972ce3d2c3bebedd7ff1900b3cf2ef5745
Renaud JANET <renaud.janet@etu.sorbonne-universite.fr>
2025-11-01 17:27:34 +0100

    Nettoyage : déplacement des dossiers ROS dans EWheelChAIr_ws/ et ajout de 3d_models/

--- src/sabertooth_controller/build/sabertooth_controller/share/sabertooth_controller/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/media/renaud/Blade 15 HDD/M2_Cours/Designing/sabertooth_ws/src/sabertooth_controller/build/sabertooth_controller"
