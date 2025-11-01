8cee0bb65815716212ef007289f3b2ba645687f9
Renaud JANET <renaud.janet@etu.sorbonne-universite.fr>
2025-11-01 17:44:38 +0100

    Ajout du contenu de EWheelChAIr_ws/

---
+++ EWheelChAIr_ws/.history/src/arduino_interface/setup_20251030154056.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'arduino_interface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='renaud',
+    maintainer_email='renaud@todo.todo',
+    description='Package to read the data from Arduino and publish it on a ROS2 topic',
+    license='Apache License 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            
+        ],
+    },
+)
---
+++ EWheelChAIr_ws/build/arduino_interface/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/media/renaud/Blade 15 HDD/M2_Cours/Designing/E-WheelChAIr/EWheelChAIr_ws/install/arduino_interface'
