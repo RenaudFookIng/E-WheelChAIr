bb84f1aa998ac895270dfbea94e909c1b4498e5b
RenaudErgoIng <165072816+RenaudFookIng@users.noreply.github.com>
2026-01-23 21:37:03 +0100

    launch servo-joystick

--- src/e_wheelchair_launch/setup.py
+++ src/e_wheelchair_launch/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
-from glob import glob
 import os
+from glob import glob
 
 package_name = 'e_wheelchair_launch'
 
@@ -9,17 +9,19 @@ setup(
     version='0.1.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch',
-            glob(os.path.join(package_name, 'launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join(package_name, 'launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Renaud JANET',
+    maintainer='Renaud Janet',
     maintainer_email='renaud.janet@etu.sorbonne-universite.fr',
     description='Launch files for E-WheelChAIr project',
     license='Apache License 2.0',
     tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
 )
